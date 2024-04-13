d54b50f7117fdd80454a06680ec2ff95d4374c7f
w1la <w1la920@gmail.com>
2024-04-13 13:52:58 +0300

    задание 20

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -18,13 +23,22 @@ public class ApplicationManager {
   private HibernateHelper hbm;
   private Properties properties;
 
-  public void init(String browser, Properties properties) {
+  public void init(String browser, Properties properties) throws MalformedURLException {
     this.properties = properties;
       if (driver == null) {
+        var seleniumServer = properties.getProperty("seleniumServer");
       if ("firefox".equals(browser)) {
-        driver = new FirefoxDriver();
+        if (seleniumServer!=null) {
+          driver = new RemoteWebDriver(new URL(seleniumServer),new FirefoxOptions());
+        } else {
+          driver = new FirefoxDriver();
+        }
       } else if ("chrome".equals(browser)) {
-        driver = new ChromeDriver();
+        if (seleniumServer!=null) {
+          driver = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+        } else {
+          driver = new ChromeDriver();
+        }
       } else {
         throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
       }
